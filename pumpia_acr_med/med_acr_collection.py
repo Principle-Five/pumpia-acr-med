ca843cbb1567ae7f154e1cb6b7627e5eecce39ea
Zack <zravetz@gmail.com>
2025-03-11 16:47:38 +0000

    Made SNR module
    Changes to be committed:
    	new file:   Testing/run_collection.py
    	new file:   pumpia_acr_med/med_acr_collection.py
    	new file:   pumpia_acr_med/modules/sub_snr.py

---
+++ pumpia_acr_med/med_acr_collection.py
@@ -0,0 +1,37 @@
+"""
+Collection for Medium ACR with repeat images.
+"""
+
+from pumpia.module_handling.module_collections import (OutputFrame,
+                                                       BaseCollection)
+from pumpia.module_handling.in_outs.viewer_ios import MonochromeDicomViewerIO
+from pumpia.widgets.viewers import BaseViewer
+
+from .modules.sub_snr import SubSNR
+from .acr_med_context import MedACRContextManagerGenerator
+
+
+class MedACRrptCollection(BaseCollection):
+    context_manager_generator = MedACRContextManagerGenerator()
+
+    viewer1 = MonochromeDicomViewerIO(row=0, column=0)
+    viewer2 = MonochromeDicomViewerIO(row=0, column=1)
+
+    snr = SubSNR(verbose_name="SNR")
+
+    snr_output = OutputFrame(verbose_name="SNR Output")
+
+    def load_outputs(self):
+        self.snr_output.register_output(self.snr.signal)
+        self.snr_output.register_output(self.snr.noise)
+        self.snr_output.register_output(self.snr.SNR)
+
+    def on_image_load(self, viewer: BaseViewer) -> None:
+        if viewer is self.viewer1:
+            if self.viewer1.image is not None:
+                image = self.viewer1.image
+                self.snr.viewer1.load_image(image)
+        elif viewer is self.viewer2:
+            if self.viewer2.image is not None:
+                image = self.viewer2.image
+                self.snr.viewer2.load_image(image)
