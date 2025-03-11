7f7b96720fdb4e28d72fcedf2295d01af0aee5e4
Zack Ravetz <zravetz@gmail.com>
2025-03-11 21:34:54 +0000

    Added corrections to SNR
    Changes to be committed:
    	modified:   pumpia_acr_med/med_acr_collection.py
    	modified:   pumpia_acr_med/modules/sub_snr.py

--- pumpia_acr_med/modules/sub_snr.py
+++ pumpia_acr_med/modules/sub_snr.py
@@ -3,13 +3,19 @@ Subtraction SNR module for medium ACR phantom
 """
 
 import numpy as np
+import math
 
 from pumpia.module_handling.modules import PhantomModule
 from pumpia.module_handling.in_outs.roi_ios import InputEllipseROI
 from pumpia.module_handling.in_outs.viewer_ios import MonochromeDicomViewerIO
-from pumpia.module_handling.in_outs.simple import PercInput, FloatOutput, IntOutput
+from pumpia.module_handling.in_outs.simple import (PercInput,
+                                                   FloatInput,
+                                                   BoolInput,
+                                                   FloatOutput,
+                                                   IntOutput)
 from pumpia.image_handling.roi_structures import EllipseROI
 from pumpia.file_handling.dicom_structures import Series, Instance
+from pumpia.file_handling.dicom_tags import MRTags
 
 from ..acr_med_context import MedACRContextManagerGenerator, MedACRContext
 
@@ -20,11 +26,21 @@ class SubSNR(PhantomModule):
     viewer2 = MonochromeDicomViewerIO(row=0, column=1, allow_changing_rois=False)
 
     size = PercInput(70, verbose_name="Size (%)")
+    ref_bandwidth = FloatInput(1, verbose_name="Reference Bandwidth (Hz/px)")
+    bw_cor_bool = BoolInput(verbose_name="Bandwidth Correction")
+    pix_size_bool = BoolInput(verbose_name="Pixel Size Correction")
+    avg_cor_bool = BoolInput(verbose_name="Averages Correction")
+    os_cor_bool = BoolInput(verbose_name="Oversampling Correction")
 
     slice_used = IntOutput()
+    im_bw = FloatOutput(verbose_name="Image Bandwidth (Hz/px)")
+    pixel_size_cor = FloatOutput(verbose_name="Pixel Size Correction")
+    oversampling_cor = FloatOutput(verbose_name="Oversampling Correction")
+    avg_cor = FloatOutput(verbose_name="Averages Correction")
     signal = FloatOutput()
     noise = FloatOutput()
-    SNR = FloatOutput(verbose_name="SNR")
+    snr = FloatOutput(verbose_name="SNR")
+    cor_snr = FloatOutput(verbose_name="Corrected SNR")
 
     signal_roi1 = InputEllipseROI("SNR ROI1")
     signal_roi2 = InputEllipseROI("SNR ROI2", allow_manual_draw=False)
@@ -89,9 +105,64 @@ class SubSNR(PhantomModule):
             sum_roi = np.mean(roi_sum)
             if isinstance(sum_roi, float):
                 self.signal.value = sum_roi
-            roi_noise = np.std(roi_sub) / np.sqrt(2)
+            roi_noise = np.std(roi_sub) / math.sqrt(2)
             if isinstance(roi_noise, float):
                 self.noise.value = roi_noise
             snr = sum_roi / roi_noise
             if isinstance(snr, float):
-                self.SNR.value = snr
+                self.snr.value = snr
+
+            cor_snr = snr
+
+            image = roi1.image
+            if isinstance(image, Instance):
+                px_cor = 1
+                bw_cor = 1
+                avg_cor = 1
+                os_cor = 1
+
+                if self.pix_size_bool.value:
+                    pix_size = image.pixel_size
+                    px_cor = 1 / math.prod(pix_size)
+                    self.pixel_size_cor.value = px_cor
+
+                if self.bw_cor_bool.value:
+                    ref_bw = self.ref_bandwidth.value
+                    try:
+                        im_bw = image.get_tag(MRTags.PixelBandwidth)
+                        try:
+                            im_bw = float(im_bw)  # type: ignore
+                        except (ValueError, TypeError):
+                            im_bw = ref_bw
+                    except KeyError:
+                        im_bw = ref_bw
+                    self.im_bw.value = im_bw
+                    bw_cor = math.sqrt(im_bw / ref_bw)
+
+                if self.avg_cor_bool.value:
+                    try:
+                        im_av = image.get_tag(MRTags.NumberOfAverages)
+                        try:
+                            im_av = float(im_av)  # type: ignore
+                        except (ValueError, TypeError):
+                            im_av = 1
+                    except KeyError:
+                        im_av = 1
+                    avg_cor = 1 / math.sqrt(im_av)
+                    self.avg_cor.value = avg_cor
+
+                if self.os_cor_bool.value:
+                    try:
+                        im_os = image.get_tag(MRTags.OversamplingPhase)
+                        try:
+                            im_os = float(im_os)  # type: ignore
+                        except (ValueError, TypeError):
+                            im_os = 0
+                    except KeyError:
+                        im_os = 0
+                    os_cor = 1 / math.sqrt(1 + im_os)
+                    self.oversampling_cor.value = os_cor
+
+                cor_snr = snr * px_cor * bw_cor * avg_cor * os_cor
+
+            self.cor_snr.value = float(cor_snr)
