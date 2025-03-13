dd1bd36d84286711e556d09af14285da3366b9fe
Zack <zravetz@gmail.com>
2025-03-13 16:20:44 +0000

    Changes to be committed: 	modified:   pumpia_acr_med/med_acr_collection.py 	new file:   pumpia_acr_med/modules/slice_pos.py 	new file:   pumpia_acr_med/modules/slice_width.py 	modified:   pumpia_acr_med/modules/uniformity.py

---
+++ pumpia_acr_med/modules/slice_pos.py
@@ -0,0 +1,5 @@
+# in mm
+ROIs_offset_toward_top = 65
+ROIs_width = 4
+ROIs_height = 20
+# offset 1 and -5
---
+++ pumpia_acr_med/modules/slice_width.py
@@ -0,0 +1,4 @@
+# ROI sizes in mm
+ROI_HEIGHT = 4
+ROI_WIDTH = 100
+# ROIs height 1 away and -4 away from centre
