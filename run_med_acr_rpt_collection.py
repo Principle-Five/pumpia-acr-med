55f3f816da75a2366bbf9749493036f5c82a7948
Zack <150851679+zravetz@users.noreply.github.com>
2025-10-23 19:21:09 +0100

    Fixed nuitka build for compressed dicoms
    Changes to be committed:
    	new file:   dicoms.nuitka-package.config.yml
    	modified:   run_med_acr_rpt_collection.py

--- run_med_acr_rpt_collection.py
+++ run_med_acr_rpt_collection.py
@@ -1,25 +1,12 @@
 # command line "python -m nuitka .\run_med_acr_rpt_collection.py"
 
-# nuitka-project-if: {OS} in ("Windows", "Linux", "Darwin", "FreeBSD"):
-#    nuitka-project: --onefile
-# nuitka-project-else:
-#    nuitka-project: --mode=onefile
+# tk-inter is needed for tkinter to work with nuitka
+# nuitka documentation suggests it is not explicitly needed/found automatically but this is wrong
+# (possibly due to inclusion of matplotlib)
 
 # nuitka-project: --enable-plugin=tk-inter
-
-# PyDICOM decoders must be manually included
-# nuitka-project: --include-module=pydicom.pixels.decoders.base
-# nuitka-project: --include-module=pydicom.pixels.decoders.gdcm
-# nuitka-project: --include-module=pydicom.pixels.decoders.pillow
-# nuitka-project: --include-module=pydicom.pixels.decoders.pyjpegls
-# nuitka-project: --include-module=pydicom.pixels.decoders.pylibjpeg
-# nuitka-project: --include-module=pydicom.pixels.decoders.rle
-# nuitka-project: --include-module=pydicom.pixels.encoders.base
-# nuitka-project: --include-module=pydicom.pixels.encoders.gdcm
-# nuitka-project: --include-module=pydicom.pixels.encoders.native
-# nuitka-project: --include-module=pydicom.pixels.encoders.pyjpegls
-# nuitka-project: --include-module=pydicom.pixels.encoders.pylibjpeg
-# nuitka-project: --include-package-data=pydicom
+# nuitka-project: --mode=onefile
+# nuitka-project: --user-package-configuration-file=dicoms.nuitka-package.config.yml
 
 from pumpia_acr_med.med_acr_rpt_collection import MedACRrptCollection
 
